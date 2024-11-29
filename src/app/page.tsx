05f6550ef9c413f717110767372b5a81259afea7
Garrett Bennett <46851255+gbennett00@users.noreply.github.com>
2024-11-29 14:59:44 -0700

    Demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Push to dev branch to redeploy? (pls work)</li>
+          <li>This is the main branch</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
